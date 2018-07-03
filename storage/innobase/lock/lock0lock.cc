d1659a7d23a9f24d9db5ae11e4d128e234b2b40e
Jiamin Huang <hjmsens@gmail.com>
2018-07-03 11:27:38 -0400

    Remove the extra tabular(not needed).

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -788,10 +788,10 @@ lock_sys_close(void)
 	create_stat_file_if_not_exists();
 	std::ofstream outfile("../stats", std::ofstream::out | std::ofstream::app);
 	if (ORIGINAL) {
-		outfile << "ORIGINAL\t\t" << total_wait_time << "\t\t"
+		outfile << "ORIGINAL\t" << total_wait_time << "\t\t"
 						<< num_waits << "\t\t" << largest_popular_size << std::endl;
 	} else {
-		outfile << POPULARITY_THRESHOLD << "\t\t" << total_wait_time << "\t\t"
+		outfile << POPULARITY_THRESHOLD << '\t' << total_wait_time << "\t\t"
 						<< num_waits << "\t\t" << largest_popular_size << std::endl;
 	}
 }
